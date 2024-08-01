518ec61bb6a04629bf201db6dde7995e88680451
KhaledAbdellaty <kabdellaty@gmail.com>
2024-08-01 14:59:45 +0300

    4. Read and filter data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -67,3 +67,27 @@ class RedactingFormatter(logging.Formatter):
         filtered = filter_datum(self.fields,
                                 self.REDACTION, message, self.SEPARATOR)
         return filtered
+
+
+def main():
+    """the Entry point"""
+    fields = "name,email,phone,ssn,password,ip,last_login,user_agent"
+    columns = fields.split(',')
+    query = "SELECT {} FROM users;".format(fields)
+    info_logger = get_logger()
+    connection = get_db()
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        for row in rows:
+            record = map(
+                lambda x: '{}={}'.format(x[0], x[1]),
+                zip(columns, row),)
+            msg = '{};'.format('; '.join(list(record)))
+            args = ("user_data", logging.INFO, None, None, msg, None, None)
+            log_record = logging.LogRecord(*args)
+            info_logger.handle(log_record)
+
+
+if __name__ == "__main__":
+    main()

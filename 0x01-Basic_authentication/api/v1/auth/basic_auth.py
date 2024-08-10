b68f7350673796173438d414a6f7aa1df76a242a
KhaledAbdellaty <kabdellaty@gmail.com>
2024-08-10 03:15:12 +0300

    8. Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@ BasicAuth Module.
 """
 from .auth import Auth
 import re
+import base64
 
 
 class BasicAuth(Auth):
@@ -17,9 +18,25 @@ class BasicAuth(Auth):
         """A function that returns the Base64 part of
         the Authorization header for a Basic Authentication.
         """
-        if type(authorization_header) == str:
+        if isinstance(authorization_header, str):
             pattern = r'Basic (?P<token>.+)'
             field_match = re.fullmatch(pattern, authorization_header.strip())
             if field_match is not None:
                 return field_match.group('token')
         return None
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str) -> str:
+        """A function that returns the decoded value
+        of a Base64 string
+        """
+        if isinstance(base64_authorization_header, str):
+            try:
+                res = base64.b64decode(
+                    base64_authorization_header,
+                    validate=True
+                )
+                return res.decode('utf-8')
+            except UnicodeDecodeError:
+                return None

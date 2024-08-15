ed419933ef7b86b20cfd809168feefb1fb59906a
KhaledAbdellaty <kabdellaty@gmail.com>
2024-08-15 20:01:53 +0300

    8. Credentials validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -38,7 +38,7 @@ class Auth:
         try:
             user = self._db.find_user_by(email=email)
             return bcrypt.checkpw(
-                password.encode("utf-8"),
+                password.encode('utf-8'),
                 user.hashed_password,
             )
         except Exception:

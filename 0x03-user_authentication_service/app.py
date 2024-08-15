8dd0bad176d105742426622b0840306291117f18
KhaledAbdellaty <kabdellaty@gmail.com>
2024-08-15 18:54:10 +0300

    1. create user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 """Flask App"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+from sqlalchemy.orm.exc import NoResultFound
 
 
+AUTH = Auth()
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
     return jsonify({"message": "Bienvenue"})
+
+@app.route('/users', methods=['POST'])
+def user_register():
+    email, password = request.form.get("email"), request.form.get("password")
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":

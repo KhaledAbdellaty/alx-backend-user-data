0fefe88ead5f5223ad59fdce7960e45cdc9f87cd
KhaledAbdellaty <kabdellaty@gmail.com>
2024-08-16 18:11:40 +0300

    1. Empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+A SessionAuth Module class that inherits from Auth.
+"""
+from auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    A SessionAuth class that inherits from Auth.
+    """

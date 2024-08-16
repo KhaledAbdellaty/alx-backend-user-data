c3b67d893e2a5457610cf8be7c9a2687298dce96
KhaledAbdellaty <kabdellaty@gmail.com>
2024-08-16 16:24:45 +0300

    fix errors

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -57,13 +57,12 @@ def logout() -> str:
     if "session_id" in request.cookies:
         print('Yes')
     session_id = request.cookies.get("session_id")
-    if session_id:
-        user = AUTH.get_user_from_session_id(session_id)
-        if user is None:
-            abort(403)
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    else:
         AUTH.destroy_session(user.id)
         return redirect("/")
-    return redirect("/")
 
 
 @app.route("/profile", methods=["GET"], strict_slashes=False)
